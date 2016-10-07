4b7244bd5d217c2bd996392c7f91d9389bc3ff4a
zlnoek <evangelion_unit-01@hotmail.com>
2016-10-07 04:27:37 -0700

    solve toy problem #1

---
+++ work.js
@@ -0,0 +1,14 @@
+// PROMPT: Write a for loop than can loop thru any array
+
+var newArray = [1,2,3,4,5];
+
+// for every element double it
+for (var i=0; i < newArray.length; i++){
+  // i=0, i = index of array
+  newArray[i] = newArray[i] * 2;
+}
+
+console.log(newArray);
+
+// Start On Underbar Functions
+// http://underscorjs.org/
