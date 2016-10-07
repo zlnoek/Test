6c02100dff8c2167c8dcdb7350158f9d528c1488
zlnoek <evangelion_unit-01@hotmail.com>
2016-10-07 04:58:48 -0700

    implementing each function

--- work.js
+++ work.js
@@ -1,5 +1,5 @@
 // PROMPT: Write a for loop than can loop thru any array
-
+/*
 var newArray = [1,2,3,4,5];
 
 // for every element double it
@@ -9,6 +9,19 @@ for (var i=0; i < newArray.length; i++){
 }
 
 console.log(newArray);
-
+*/
 // Start On Underbar Functions
-// http://underscorjs.org/
+// http://underscorejs.org/
+
+// PROMPT: Implement the each function. Each function will take in collection and callback
+// as arguments. It will apply the callback to every item in the collection. The callback
+// needs to be able to taking in the item, index, and collection as the arguments.
+
+// Example collection: [1,2,3,4,5]
+// Example callback: function(item){ return item * 2;}
+// Collection will now be: [2,4,6,8,10]
+function each(collection, callback){
+  for (var i = 0; i < collection.length; i++){
+    callback(collection[i], i, collection);
+  }
+}
